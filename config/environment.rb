654f1410e3886151377b7fec2027aedc696ef4f0
Tim Novinger <tim.novinger@gmail.com>
2010-01-10 15:11:38 -0500

    added acts_as_list and acts_as_tree to environment.rb, unpacked into vendors

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.4' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -20,6 +20,8 @@ Rails::Initializer.run do |config|
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
   config.gem 'typus', :source => 'http://gemmcutter.org'
+  config.gem 'acts_as_list'
+  config.gem 'acts_as_tree'
   config.gem 'kete-tiny_mce', :lib => 'tiny_mce', :source => 'http://gems.github.com'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
