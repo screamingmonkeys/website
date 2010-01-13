9bbf0da1e447e39f2dee72a56e33ead1c181e489
Tim Novinger <tim.novinger@gmail.com>
2010-01-13 14:18:56 -0500

    added logic to route to 404 page correctly

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,26 +4,30 @@ class PagesController < ApplicationController
   require 'htmlentities'
 
   def show
-    @page   = Page.find_by_permalink( params[:permalink] )
     @tabs   = Page.all
-    @images = @page.images
-    @rss    = ''
+    @page   = Page.find_by_permalink( params[:permalink] )
+    
+    if @page.nil?
+      render :file => "#{RAILS_ROOT}/public/404.html", :layout => false, :status => 404
+    else
+    
+      @images = @page.images
+      @rss    = ''
    
-    if @page.title == 'Schedule'
-      feed_url = 'http://api.meetup.com/events.rss/?zip=46815&group_urlname=screamingmonkeys&key=555f7b666820756c1a382f13a6b2b7e'
-#     feed_url = 'http://www.meetup.com/screamingmonkeys/calendar/rss/'
+      if @page.title == 'Schedule'
+        coder = HTMLEntities.new
+        feed_url = 'http://api.meetup.com/events.rss/?zip=46815&group_urlname=screamingmonkeys&key=555f7b666820756c1a382f13a6b2b7e'
 
-      coder = HTMLEntities.new
-
-      open(feed_url) do |http|
-        response = http.read
-        result   = RSS::Parser.parse(response, false)
-        result.items.each do |item| 
-          description = coder.decode(item.description)
-          @rss += "<h2>## #{item.title}</h2> <p>#{description}</p> <p>=> <a href='#{item.link}'>view on meetup.com</a></p>"
+        open(feed_url) do |http|
+          response = http.read
+          result   = RSS::Parser.parse(response, false)
+          result.items.each do |item| 
+            description = coder.decode(item.description)
+            @rss += "<h2>## #{item.title}</h2> <p>#{description}</p> <p>=> <a href='#{item.link}'>view on meetup.com</a></p>"
+          end
         end
-      end
-      @rss = '<div id="rss">' + @rss + '</div>'
+        @rss = '<div id="rss">' + @rss + '</div>'
+       end
      end
   end
 end
