2692db9dd17c73f2ad579e9aa057b081d4ddaabd
Tim Novinger <tim.novinger@gmail.com>
2010-01-12 20:05:44 -0500

    refactored helper methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,24 +1,10 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
-  def copyright_date
-    launch_year   = '2008'
-    current_year  = Time.now.year.to_s
-  
-    launch_year +"-"+ current_year
-  end
-  
   def bodytag_id
-    a = controller.action_name.underscore
-    
-    #change underscores to spaces and capitalize words in string
-    "#{a}".gsub(/_/, '-')
+    @page.permalink
   end
   
   def is_current_tab
-    if @tab.permalink == @page.permalink 
-      'class="current"'
-    else
-      ''
-    end
+    @tab.permalink == @page.permalink ? 'class="current"' : ''
   end
 end
