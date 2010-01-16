f3ccf8e37c08daa0d17a69f4488703796affa5ac
Tim Novinger <tim.novinger@gmail.com>
2010-01-16 01:39:35 -0500

    code beautifing and refractoring

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,27 +7,10 @@ class PagesController < ApplicationController
     @tabs   = Page.all
     @page   = Page.find_by_permalink( params[:permalink] )
     
-    if @page.nil?
-      @page = Page.find_by_permalink( '404' )
-    else
-    
+    unless @page.nil?
       @images = @page.images
-      @rss    = ''
-   
-      if @page.title == 'Schedule'
-        coder = HTMLEntities.new
-        feed_url = 'http://api.meetup.com/events.rss/?zip=46815&group_urlname=screamingmonkeys&key=555f7b666820756c1a382f13a6b2b7e'
-
-        open(feed_url) do |http|
-          response = http.read
-          result   = RSS::Parser.parse(response, false)
-          result.items.each do |item| 
-            description = coder.decode(item.description)
-            @rss += "<h2>## #{item.title}</h2> <p>#{description}</p> <p>=> <a href='#{item.link}'>view on meetup.com</a></p>"
-          end
-        end
-        @rss = '<div id="rss">' + @rss + '</div>'
-       end
-     end
-  end
+    else  
+      @page = Page.find_by_permalink("404")
+    end
+  end   
 end
--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,38 @@
 module PagesHelper
+  def page_nav
+    html = ''
+    @tabs.each do |@tab|
+      unless @tab.permalink == '404'
+	      html += "<li><a href='/#{@tab.permalink}' #{is_current_tab}>#{@tab.title}</a></li>"
+	    end
+	  end
+    "<ul>#{html}</ul>"
+  end
+  
+  def page_images
+    html = ''
+    @page.images.split(',').each do |img|
+       html += "<li><img src='#{img}' alt='' class='border' /></li>"
+    end
+    "<ul>#{html}</ul>"
+  end
+  
+  def rss_feed
+     html = ''
+
+     if @page.permalink == "schedule"
+       coder    = HTMLEntities.new
+       feed_url = "http://api.meetup.com/events.rss/?zip=46815&group_urlname=screamingmonkeys&key=555f7b666820756c1a382f13a6b2b7e"
+
+       open(feed_url) do |http|
+         response = http.read
+         result   = RSS::Parser.parse(response, false)
+         result.items.each do |item| 
+           description = coder.decode(item.description)
+           html += "<h2>## #{item.title}</h2> <p>#{description}</p> <p>=> <a href='#{item.link}'>view on meetup.com</a></p>"
+         end
+       end
+       "<div id='rss'>#{html}</div>"
+      end
+   end
 end
