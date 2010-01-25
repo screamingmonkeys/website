9f67de47ee1e80e1cde5c0b7782f6d6c61afa863
Tim Novinger <tim.novinger@gmail.com>
2010-01-25 09:25:07 -0500

    finished feed model, tested ok

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -9,16 +9,11 @@ module PagesHelper
   end
   
   def rss_feed 
-     # Default text
-     html = "<h2>## Event Details</h2> 
-           <p>Sorry, but we do not have any events scheduled at this time.</p>
-           <p>Please check back later.</p>"
-
      # Pull RSS feed and parse
      if @page.permalink == "schedule"
-       @feed.get_data("http://api.meetup.com/events.rss/?zip=46815&group_urlname=screamingmonkeys&key=555f7b666820756c1a382f13a6b2b7e")
+       schedule_feed = @feed.get_data("http://api.meetup.com/events.rss/?zip=46815&group_urlname=screamingmonkeys&key=555f7b666820756c1a382f13a6b2b7e")
 
-       "<div id='rss'>#{@feed.html}</div>"
+       "<div id='rss'>#{schedule_feed}</div>"
      end
    end
    
--- app/models/feed.rb
+++ app/models/feed.rb
@@ -2,26 +2,27 @@ class Feed
   require 'rss/2.0'
   require 'open-uri'
   require 'htmlentities'
-
-  attr_reader :html
-  
-  def initialize
-    @html = ''
-  end
   
   def get_data(url)
+    # Default text
+    feed_data = "<h2>## Event Details</h2> 
+                 <p>Sorry, but we do not have any events scheduled at this time.</p>
+                 <p>Please check back later.</p>"
+                         
     unless url.empty?
       coder    = HTMLEntities.new
       open(url) do |http|
         response = http.read
         result   = RSS::Parser.parse(response, false)
-      
+
         result.items.each do |item| 
           description = coder.decode(item.description)
-          @html += "<h2>## #{item.title}</h2> <p>#{description}</p> <p>=> <a href='#{item.link}'>view on meetup.com</a></p>"
+          feed_data += "<h2>## #{item.title}</h2> <p>#{description}</p> <p>=> <a href='#{item.link}'>view on meetup.com</a></p>"
         end
+        puts result
       end
     end
+    feed_data
   end
   
 end
