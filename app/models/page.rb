af2487ba817585f5d0343a00b52b86f0923b60c1
Tim Novinger <tim.novinger@gmail.com>
2010-09-08 22:57:06 -0400

    Feature: added textile input to admin and styled
    - Updated CSS styling to better accommodate content generated from
      textile output
    
    - Added acts_as_textile plugin and updated Models as necessary

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,3 +1,4 @@
 class Page < ActiveRecord::Base
+  acts_as_textiled :content
   default_scope :order => 'sort_by DESC'
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ActiveRecord::Base
+  acts_as_textiled :content
+  
   default_scope :order => "created_at DESC"
   named_scope :recent_ten, :order => "created_at DESC LIMIT 10"
 end
